0188e9329b738f524f8e5c911daf5b415f5baec8
hiro_from_22 <ore097miki@gmail.com>
2021-01-27 00:41:03 +0900

    #1 alt属性が表示されないようにしました

--- src/App.js
+++ src/App.js
@@ -4,19 +4,26 @@ const App = () => {
   const fileReader = new FileReader();
   const showDataURI = () => {
     let file = document.getElementById('file');
+    let viewer = document.getElementById('viewer');
     let input = file.files[0];
     fileReader.readAsDataURL(input);
     fileReader.onload = () => {
-      document.getElementById('viewer').src = fileReader.result;
+      viewer.classList.toggle('unvisible');
+      viewer.src = fileReader.result;
     }
   };
 
   return(
     <>
       <form id="select-your-image">
-        <input type="file" id="file" onChange={showDataURI}/>
+        <input type="file" id="file" onChange={showDataURI} />
       </form>
-      <img id="viewer" alt="viewer"/>
+      <img id="viewer" alt="viewer" class="unvisible"/>
+      <style jsx>{`
+        .unvisible{
+          opacity: 0;
+        }
+      `}</style>
     </>
   );
 };
