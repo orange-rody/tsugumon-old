5e6db3d173dafc3506a88c2dad4bc400ce810bdc
hiro_from_22 <ore097miki@gmail.com>
2021-01-26 23:07:19 +0900

    #1 選択した画像を画面上に表示する機能を実装しました

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  const fileReader = new FileReader();
+  const showDataURI = () => {
+    let file = document.getElementById('file');
+    let input = file.files[0];
+    fileReader.readAsDataURL(input);
+    fileReader.onload = () => {
+      document.getElementById('viewer').src = fileReader.result;
+    }
+  };
+
+  return(
+    <>
+      <form id="select-your-image">
+        <input type="file" id="file" onChange={showDataURI}/>
+      </form>
+      <img id="viewer" alt="viewer"/>
+    </>
   );
-}
+};
 
 export default App;
