f07106296c73a5df60292b5434c116a74e0fa819
hiro_from_22 <ore097miki@gmail.com>
2021-01-27 23:56:36 +0900

    #1-camera-roll 表示される画像のサイズを調整しました

--- src/App.js
+++ src/App.js
@@ -4,26 +4,33 @@ const App = () => {
   const fileReader = new FileReader();
   const showDataURI = () => {
     let file = document.getElementById('file');
-    let viewer = document.getElementById('viewer');
     let input = file.files[0];
+    let cv = document.getElementById('cv');
+    let c = cv.getContext('2d');
     fileReader.readAsDataURL(input);
     fileReader.onload = () => {
-      viewer.classList.toggle('unvisible');
-      viewer.src = fileReader.result;
+      let img = new Image();
+      img.setAttribute('src',fileReader.result);
+      img.onload = () => {
+        let width = img.width;
+        let height = img.height;
+        if(width > cv.maxWidth){          
+          height = Math.round(height * cv.maxWidth / width);
+          width = cv.maxWidth;
+        }
+        c.drawImage(img,0,0,width,height);
+      };     
     }
   };
+
+  
 
   return(
     <>
       <form id="select-your-image">
         <input type="file" id="file" onChange={showDataURI} />
       </form>
-      <img id="viewer" alt="viewer" class="unvisible"/>
-      <style jsx>{`
-        .unvisible{
-          opacity: 0;
-        }
-      `}</style>
+      <canvas id = "cv" width = "400" height = "400" alt= "canvas" />
     </>
   );
 };
