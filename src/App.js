34671eaa40e423d424ccb47c633f68789e490dad
hiro_from_22 <ore097miki@gmail.com>
2021-02-02 02:22:44 +0900

    #1 テキストエリアの追加とCSSの調整を行いました

--- src/App.js
+++ src/App.js
@@ -11,42 +11,13 @@ export default function Preview() {
 
   return(
     <>
-      <label htmlFor="fileElem" id="fileSelect">画像を選択</label>
-      <style jsx>{`
-        #fileSelect{
-          display: block;
-          width: 150px;
-          height: 40px;
-          border-radius: 20px;
-          margin-top: 20px;
-          text-align: center;
-          line-height: 40px;
-          color: hsl(15, 70%, 54%);
-          background-color: hsl(42, 82%, 83%);
-        }
-      `}</style>
-      <input type = "file" id = "fileElem" onChange = {handleChangeFile} />
-      <style jsx>{`
-          #fileElem {
-            display: none;
-          }
-      `}</style>
-      <div id="imageWrap">
-      {/* <style jsx> {`
-        #imageWrap {
-          width: 100%;
-          background-image: url("../public/no_image_square.jpg");
-          background-size: cover;
-        }
-      `}</style> */}
+      <form>
+        <label htmlFor="fileElem" id="fileSelect">画像を選択</label>
+        <input type = "file" id = "fileElem" onChange = {handleChangeFile} />
         <img src = {preview} id = "preview" alt = "画像のプレビュー"/>
-        {/* <style jsx>{`
-          #preview {
-            width: 100%;
-            padding-top: 10%;
-          }
-        `}</style> */}
-      </div>
+        <textarea id="caption" placeholder = "コメントを入力してください"></textarea>
+        <input type="submit" id="submit" value = "投稿する"/>
+      </form>
     </>
   )
 }
