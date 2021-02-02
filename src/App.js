8d40c35b93f308f351703f40bbbb13ec1515c4ab
hiro_from_22 <ore097miki@gmail.com>
2021-02-02 23:53:05 +0900

    #1 画面レイアウトを調整しました

--- src/App.js
+++ src/App.js
@@ -14,7 +14,9 @@ export default function Preview() {
       <form>
         <label htmlFor="fileElem" id="fileSelect">画像を選択</label>
         <input type = "file" id = "fileElem" onChange = {handleChangeFile} />
-        <img src = {preview} id = "preview" alt = "画像のプレビュー"/>
+        <div id = "previewWrap">
+          <img src = {preview} id = "preview" alt = "画像のプレビュー"/>
+        </div>
         <textarea id="caption" placeholder = "コメントを入力してください"></textarea>
         <input type="submit" id="submit" value = "投稿する"/>
       </form>
